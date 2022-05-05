cacdac30157e434d97e3289202b751484033efdb
adityak <adityak@ADITYAK>
2022-05-05 18:18:08 +0530

    Updated

--- src/main/java/com/javatpoint/springbootdevtoolsexample/SpringBootDevtoolsExampleApplication.java
+++ src/main/java/com/javatpoint/springbootdevtoolsexample/SpringBootDevtoolsExampleApplication.java
@@ -3,6 +3,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletResponse;
+import java.sql.*;
 
 @SpringBootApplication
 public class SpringBootDevtoolsExampleApplication 
@@ -25,6 +26,32 @@ public void test(HttpServletResponse response){
 		response.addCookie(xsrfTokenCookie);
 	
 	
+}
+
+public void getUser(Connection con, String user) throws SQLException {
+
+  Statement stmt1 = null;
+  Statement stmt2 = null;
+  PreparedStatement pstmt;
+  try {
+    stmt1 = con.createStatement();
+    ResultSet rs1 = stmt1.executeQuery("GETDATE()"); // No issue; hardcoded query
+
+    stmt2 = con.createStatement();
+    ResultSet rs2 = stmt2.executeQuery("select FNAME, LNAME, SSN " +
+                 "from USERS where UNAME=" + user);  // Sensitive
+
+    pstmt = con.prepareStatement("select FNAME, LNAME, SSN " +
+                 "from USERS where UNAME=" + user);  // Sensitive
+    ResultSet rs3 = pstmt.executeQuery();
+
+    //...
+}catch(Exception ex){
+	
+}finally{
+	
+}
+
 }
 
 
